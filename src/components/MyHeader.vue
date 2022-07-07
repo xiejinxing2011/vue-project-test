86bc63f1da803f4e44e2cbe35fa2d2edea5ae2b5
ur a good person <664076179@qq.com>
2022-07-07 17:24:32 +0800

    first

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <input value="" placeholder="搜索"/>
+  </div>
+</template>
+
+<script>
+    export default {
+        name:"MyHeader",
+        
+    }
+</script>
+
+<style>
+
+</style>
