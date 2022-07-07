890b9bc3f3fe86fd18a6ed2b106d13ea21535714
ur a good person <664076179@qq.com>
2022-07-07 14:41:38 +0800

    master

---
+++ src/components/Student.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>学生姓名：{{name}}</h2>
+        <h2>学生年龄：{{age}}</h2>
+        <button @click="age++">点我年龄+1</button>
+        <button @click="addAge">点我年龄+1</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Student",
+   data() {
+    return {
+        name:"张三",
+        age:18
+    }
+   },
+methods: {
+    addAge(){
+        this.age++
+    }
+},
+}
+</script>
