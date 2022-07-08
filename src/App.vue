82f22f632c099634c35fae6cce73ad3cf0665a94
ur a good person <664076179@qq.com>
2022-07-08 14:26:51 +0800

    123

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,92 @@
 <template>
-  <div id="app">
-    
-    
+  <div class="todo-container">
+    <div class="todo-wrap">
+      <MyHeader :addItem="addItem"/>
+      <!-- <TodoList :todos="todos" :changeDone="changeDone"/> -->
+      <TodoList :todos="todos"/> 
+      <MyFooter/>
+    </div>
   </div>
 </template>
 
 <script>
   import MyHeader from './components/MyHeader'
   import MyFooter from './components/MyFooter'
-  import List from './components/List'
+  import TodoList from './components/TodoList'
   export default {
     name: 'App',
     components: {
       MyHeader,
       MyFooter,
-      List
-    }
+      TodoList
+    },
+    data(){
+        return{
+          todos:[
+            {
+              id:'001',title:'吃饭',done:true
+            },
+            {
+              id:'002',title:'睡觉',done:false
+            },
+            {
+              id:'003',title:'喝酒',done:false
+            }
+          ]
+        }
+      },
+      methods: {
+        addItem(v){
+          console.log("接收到参数：",v);
+          this.todos.unshift(v)
+        },
+        // changeDone(id){
+        //   this.todos.forEach(todo => {
+        //     if(todo.id === id){
+        //       todo.done = !todo.done
+        //     }
+        //   });
+        // }
+      },
   }
 </script>
 
 <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
+  /*base*/
+  body{
+    background: #fff;
+  }
+  .btn{
+    display: inline-block;
+    padding: 4px 12px;
+    margin-bottom: 0;
+    font-size: 14px;
+    line-height: 20px;
     text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+    vertical-align: middle;
+    cursor: pointer;
+    box-shadow: inset 0 1px 0 rgba(255,255,255,0.2) 0 1px 2px rgba(0, 0, 0, 0.05);
+    border-radius: 4px;
+  }
+  .btn-danger{
+    color: #fff;
+    background-color: #da4f49;
+    border: 1px solid #bd362f;
+  }
+  .btn-danger :hover{
+    color: #fff;
+    background-color: #bd362f;
+  }
+  .btn :focus{
+    outline: none;
+  }
+  .todo-container{
+    width:600px;
+    margin: 0 auto ;
+  }
+  .todo-container .todo-wrap{
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
   }
 </style>
